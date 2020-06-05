dc5048eaf7c75736d5e62396e38b916c81cb7307
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-06-05 22:41:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const bodyParser = require('body-parser');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,8 +24,8 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
   console.error('Error connecting to mongo', err);
 });
-app.get('/', (req, res) => {
-  res.send('Hi There!');
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your Email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
