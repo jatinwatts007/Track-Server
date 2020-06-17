174e60ccb9650e2c13545a86d1005e99aa1a5445
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-06-17 22:23:43 +0530

    Update SignupScreen.js

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,38 +1,24 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import Spacer from '../components/Spacer';
 import { Context as authContext } from '../context/authContext';
+import AuthForm from '../components/authForm';
+import NavLink from '../components/navLink';
 
 const SignupScreen = ({ navigation }) => {
   const { state, signup } = useContext(authContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  console.log(state);
+
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Trackers</Text>
-      </Spacer>
-      <Input
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
-        label="Email"
+      <AuthForm
+        headerName="Sign Up for Tracker"
+        buttonlabel="Sign up"
+        errorMessage={state.errorMessage}
+        onSubmit={signup}
       />
-      <Input
-        value={password}
-        secureTextEntry
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        label="Password"
+      <NavLink
+        text="Already have an account? click to go on Sign in Page"
+        routeName="Signin"
       />
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
-      {state.errorMessage ? <Text>{state.errorMessage}</Text> : null}
     </View>
   );
 };
@@ -48,6 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250,
+    marginTop: 100,
   },
 });
 
