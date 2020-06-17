1b851ccb69e526e86b9e7ad65c5eb859e1f3ae0a
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-06-17 22:23:10 +0530

    Update siginScreen.js

--- tracks/src/screens/siginScreen.js
+++ tracks/src/screens/siginScreen.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/authForm';
+import NavLink from '../components/navLink';
+import { Context } from '../context/authContext';
 
 const SigninScreen = () => {
+  const { state, signin } = useContext(Context);
   return (
-    <View>
-      <Text>signinScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerName="Sign In To Your Account"
+        buttonlabel="Sign in"
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+      />
+      <NavLink
+        text="Donot have an account? click to go on Sign up"
+        routeName="Signup"
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
+    marginTop: 100,
+  },
+});
 
 export default SigninScreen;
