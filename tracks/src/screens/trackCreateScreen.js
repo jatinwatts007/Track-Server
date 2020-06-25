c57359eb492eff4118dd73b14339d1f622beb984
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-06-25 22:10:13 +0530

    Update trackCreateScreen.js

--- tracks/src/screens/trackCreateScreen.js
+++ tracks/src/screens/trackCreateScreen.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync } from 'expo-location';
+import Map from '../components/map';
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+  useEffect(() => {
+    startWatching();
+  }, []);
   return (
-    <View>
-      <Text style={styles.size}>TrackCreateScreen</Text>
-    </View>
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>TrackCreateScreen</Text>
+      <Map />
+      {err ? <Text>Please grant us location access</Text> : null}
+    </SafeAreaView>
   );
 };
 
