50bc13099c9a9d57b63014c5da047abe86540f2a
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-07-02 23:43:30 +0530

    Update _mockLocation.js

--- tracks/src/_mockLocation.js
+++ tracks/src/_mockLocation.js
@@ -2,26 +2,26 @@ import * as Location from 'expo-location';
 
 const tenMetersWithDegrees = 0.0001;
 
-const getLocation = increment => {
-    return {
-        timestamp: 10000000,
-        coords:{
-            speed:0,
-            heading:0,
-            accuracy:0,
-            altitudeAccuracy:5,
-            altitude:5,
-            longitude: -122.0312186 + increment + tenMetersWithDegrees,
-            latitude: 37.33233141 + increment + tenMetersWithDegrees
-        }
-    }
+const getLocation = (increment) => {
+  return {
+    timestamp: 10000000,
+    coords: {
+      speed: 0,
+      heading: 0,
+      accuracy: 0,
+      altitudeAccuracy: 5,
+      altitude: 5,
+      longitude: 75.7793525840188 + increment + tenMetersWithDegrees,
+      latitude: 26.959472488663213 + increment + tenMetersWithDegrees,
+    },
+  };
 };
 
 let counter = 0;
 setInterval(() => {
-    Location.EventEmitter.emit('Expo.locationChanged',{
-        watchId: Location._getCurrentWatchId(),
-        location : getLocation(counter)
-    })
-   counter++;
+  Location.EventEmitter.emit('Expo.locationChanged', {
+    watchId: Location._getCurrentWatchId(),
+    location: getLocation(counter),
+  });
+  counter++;
 }, 1000);
