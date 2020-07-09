b757651dac287a28d111d2b91d098a0a084c1774
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-07-09 22:43:00 +0530

    Update trackCreateScreen.js

--- tracks/src/screens/trackCreateScreen.js
+++ tracks/src/screens/trackCreateScreen.js
@@ -1,20 +1,28 @@
 import '../_mockLocation';
 import { Context as locationContext } from '../context/locationContext';
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useCallback, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import useLocation from '../hooks/useLocation';
 import Map from '../components/map';
+import TrackForm from '../components/trackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(locationContext);
-  const [err] = useLocation(isFocused, addLocation);
+  const { state, addLocation } = useContext(locationContext);
+  const callback = useCallback(
+    (location) => {
+      addLocation(location, state.recording);
+    },
+    [state.recording],
+  );
+  const [err] = useLocation(isFocused, callback);
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>TrackCreateScreen</Text>
       <Map />
       {err ? <Text>Please grant us location access</Text> : null}
+      <TrackForm />
     </SafeAreaView>
   );
 };
