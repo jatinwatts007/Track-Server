05f8595e212a08a6c370e5279f47f92c50315b0e
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-07-11 22:46:13 +0530

    Update App.js

--- tracks/App.js
+++ tracks/App.js
@@ -11,8 +11,20 @@ import TrackCreateScreen from './src/screens/trackCreateScreen';
 import TrackListScreen from './src/screens/trackListScreen';
 import { Provider as AuthProvider } from './src/context/authContext';
 import { Provider as LocationProvider } from './src/context/locationContext';
+import { Provider as TrackProvider } from './src/context/trackContext';
 import { setNavigator } from './src/navigationRef';
 import ResolveAuthSecreen from './src/screens/resolveAuthScreen';
+import { FontAwesome } from '@expo/vector-icons';
+
+const trackListFlow = createStackNavigator({
+  TrackList: TrackListScreen,
+  TrackDetail: TrackDetailScreen,
+});
+
+trackListFlow.navigationOptions = {
+  title: 'Track',
+  tabBarIcon: <FontAwesome name="th-list" size={20} />,
+};
 
 const switchNavigator = createSwitchNavigator({
   ResolveAuth: ResolveAuthSecreen,
@@ -21,10 +33,7 @@ const switchNavigator = createSwitchNavigator({
     Signin: SigninScreen,
   }),
   mainFlow: createBottomTabNavigator({
-    trackListFlow: createStackNavigator({
-      TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen,
-    }),
+    trackListFlow,
     TrackCreate: TrackCreateScreen,
     Account: AccountScreen,
   }),
@@ -34,14 +43,16 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <LocationProvider>
-      <AuthProvider>
-        <App
-          ref={(navigator) => {
-            setNavigator(navigator);
-          }}
-        />
-      </AuthProvider>
-    </LocationProvider>
+    <TrackProvider>
+      <LocationProvider>
+        <AuthProvider>
+          <App
+            ref={(navigator) => {
+              setNavigator(navigator);
+            }}
+          />
+        </AuthProvider>
+      </LocationProvider>
+    </TrackProvider>
   );
 };
