3afc0c69fbd4499925765592427639e0f4cda42f
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-07-11 22:49:31 +0530

    Update AccountScreen.js

--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -4,6 +4,7 @@ import { Button } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import Spacer from '../components/Spacer';
 import { Context } from '../context/authContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const AccountScreen = () => {
   const { signout } = useContext(Context);
@@ -18,5 +19,10 @@ const AccountScreen = () => {
 };
 
 const styles = StyleSheet.create({});
+
+AccountScreen.navigationOptions = {
+  title: 'Account',
+  tabBarIcon: <FontAwesome name="gear" size={20} />,
+};
 
 export default AccountScreen;
